5708cc78511c13c05a8a00e4a0521eed90832c2f
Daniel Tolla <danieltda99@gmail.com>
2022-07-02 16:09:41 +0300

    fix: hr page style changed

--- client/src/pages/HR.jsx
+++ client/src/pages/HR.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Spinner from "../components/Spinner";
 import { getLeaveRequests } from "../redux/leaveSlice";
 import { Link } from "react-router-dom";
+import Table from "../components/Table";
 
 const HR = () => {
   const { leaves, isLoading } = useSelector((state) => state?.leave);
@@ -20,41 +21,37 @@ const HR = () => {
     return <Spinner />;
   }
   return (
-    <div className="w-full h-screen flex justify-center m-auto ">
-      <div className="max-w-4xl bg-gray-100 w-full p-10 mt-8">
-        <h1 className="text-center text-4xl m-10">HR Manager Page</h1>
-        <h1 className="text-center text-2xl m-10">Leave Request List</h1>
-        {leaves?.length < 1 && (
-          <>
-            <h1 className="text-center text-xl text-gray-500">
-              No Request found
-            </h1>
-          </>
-        )}
-        <List sx={{ width: "100%" }}>
-          {leaves?.map((leave, index) => (
-            <ListItem
-              key={index}
-              disableGutters
-              secondaryAction={
-                <Link to={`/hr/${leave?._id}`} className="">
-                  <button className="p-3 text-blue-600 font-semibold py-1 px-3 bg-white outline md:px-10">
-                    View Detail
-                  </button>
-                </Link>
-              }
-            >
-              <ListItemText primary={`${++index}. ${leave?.userId?.name}`} />
-              <ListItemText primary={`${++index}. ${leave?.userId?.gender}`} />
-              <ListItemText primary={` ${leave?.type}`} />
-              <ListItemText primary={` ${leave?.status}`} />
-              <ListItemText />
-              {/* <ListItemText primary={`Line New ${value}`} /> */}
-            </ListItem>
-          ))}
-        </List>
+    <>
+      <div className="w-full h-screen flex items-center justify-center m-auto p-2">
+        <div className="w-full m-auto mt-28 p-5 bg-white border rounded shadow-2xl">
+          <h1 className="text-2xl text-center">Request Leave List</h1>
+          <Table>
+            {leaves?.map((leave, index) => (
+              <tr
+                className="border-b odd:bg-white even:bg-gray-100 border-gray-50"
+                key={index}
+              >
+                <td className="py-4 px-6 text-sm">{++index}</td>
+                <td className="py-4 px-6 text-sm">{leave.userId.name}</td>
+                <td className="py-4 px-6 text-sm whitespace-nowrap">
+                  {leave.type}
+                </td>
+                <td className="py-4 px-6 text-sm whitespace-nowrap">
+                  {leave.status}
+                </td>
+                <td>
+                  <Link to={`/hr/${leave?._id}`} className="">
+                    <button className="p-3 text-blue-600 font-semibold py-1 px-3 bg-white outline md:px-10">
+                      View Detail
+                    </button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </Table>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
